1c35b93a0115a18ac1ec5bff86bc2a9c8fd75fd9
Marian Powell <marianpowell@gmail.com>
2016-10-04 17:27:14 -0400

    pulling data. need to fix styles

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+console.clear();
+
+/* Define API Key */
+var me = {
+  apiKey: 'db608e87ac40fbbcfdef8f6f6fbb0d0bea786b5f'
+};
+/* Define person whose Orgs to get */
+var username = 'addyosmani';
+
+/* Send request to Github */
+function requestOrgs() {
+  $.ajax({
+    type: 'GET',
+    url: "https://api.github.com/users/addyosmani/orgs?access_token=" + me.apiKey,
+    success: function(results) {
+      for(var org of results) {
+        var newObject = new OrganizationDetails(org);
+      }
+    },
+    error: function(jqXHR, textStatus, error) {
+      console.log(jqXHR, textStatus, error);
+    }
+  });
+}
+
+/* Constructor for OrganizationDetails */
+function OrganizationDetails(orgObject) {
+  this.info = {
+    login: orgObject.login,
+    id: orgObject.id,
+    avatar_url: orgObject.avatar_url
+  };
+  this.createElements = function() {
+    var container = $('<div>').attr('class','avatar-container');
+    var avatar = $('<img>').attr('src', this.info.avatar_url).appendTo(container);
+    container.appendTo('#orgs-section');
+  };
+  this.createElements();
+}
+
+/* link the action to the button */
+$('#load-button').on('click', function(){
+  requestOrgs();
+
+});
